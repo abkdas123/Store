71ad4b97b14dbb4fe878f2e6b73ba0ff2f53849d
shriyansh101 <shriyansh.jain303@gmail.com>
2018-05-26 14:51:20 +0530

    login integrated in solution

---
+++ MyStore/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using Models.ViewModels;
+using Services.Contract;
+using Services.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyStore.Controllers
+{
+    public class AuthenticationController : ApiController
+    {
+        private IAuthenticationService _AuthenticationService = null;
+
+        public AuthenticationController()
+        {
+            _AuthenticationService = new AuthenticationService();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/Authentication")]
+        public HttpResponseMessage Authentication(Login login)
+        {
+            HttpResponseMessage httpResponseMessage = null;
+
+            if (login != null)
+                httpResponseMessage = Request.CreateResponse(HttpStatusCode.Accepted, _AuthenticationService.AuthenticateUser(login));
+            else
+                httpResponseMessage = Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid Model");
+
+            return httpResponseMessage;
+        }
+    }
+}
