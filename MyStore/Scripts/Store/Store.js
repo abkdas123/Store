6986d8bdda3d1e018d2dc6f0fb2af1d087dda35b
abkdas123 <abkdas123@gmail.com>
2018-05-29 00:05:17 +0530

    added button to find shops

---
+++ MyStore/Scripts/Store/Store.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../Knockout/knockout-3.4.2.js" />
+var ViewModel = function () {
+
+    var self = this;
+
+    self.Stores = ko.observableArray();
+
+    self.GetAllStores = function () {
+        $.ajax
+         ({
+             type: "GET",
+             url: "/api/GetAllStore",
+             dataType: 'json',
+             async: false,
+             beforeSend: function (xhr) {
+                 xhr.setRequestHeader("Authorization", sessionStorage.getItem("token"));
+             },
+             success: function (result) {
+                 self.Stores(result);
+             },
+             error: function (exception, error, response) {
+                
+                 console.log(exception);
+                 console.log(error);
+                 console.log(response);
+             }
+         });
+    };
+
+};
+
+$(document).ready(function () {
+    var model = new ViewModel();
+    ko.applyBindings(model, document.getElementById("divStore"));
+});
